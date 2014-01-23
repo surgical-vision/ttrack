cc5b9e4b8d59b1df478156778983f5f4b42ffa0b
maximilianallan <maximilian.allan@gmail.com>
2014-01-23 14:49:02 +0000

    fixed json loader

--- include/track/model/articulated_model.hpp
+++ include/track/model/articulated_model.hpp
@@ -10,24 +10,27 @@ namespace ttrk {
 
     typedef boost::shared_ptr<ArticulatedNode> Ptr;
 
-    ci::Matrix44f getTransform();
-
-    cv::Vec3d center_of_mass_;
-    cv::Vec3d axis_of_rotation_;
+    ci::Matrix44d GetTransform();
+    void LoadData(ci::JsonTree &tree, ArticulatedNode::Ptr parent, const std::string &root_dir) ;
+    void Rotate(const double angle) { articulation_.rotate(Axis(),angle); }
+    ci::Vec3d Axis() { return axis_of_rotation_; }
+    
+    
+    ci::Vec3d center_of_mass_;
+    ci::Vec3d axis_of_rotation_;
 
     //bool direction_; (use negative angles)
     bool movable_;
 
-    ci::Matrix44f	transform_; //const
-    ci::Matrix44f articulation_;
+    ci::Matrix44d	transform_; //const
+    ci::Matrix44d articulation_;
+    double min_angle_;
+    double max_angle_;
 
     ArticulatedNode::Ptr parent_;
     std::vector<ArticulatedNode::Ptr> children_;
 
-    ci::TriMesh model_;
-
-    void LoadData(ci::JsonTree &tree, ArticulatedNode::Ptr parent) {};
-    void Rotate(const double angle) { articulation_.rotate(angle); }
+    ci::TriMesh model_;   
 
   };
 
@@ -36,7 +39,7 @@ namespace ttrk {
 
   public:
 
-    ArticulatedTree() {}
+    ArticulatedTree() : root_node_(new ArticulatedNode()) {}
     ArticulatedNode::Ptr RootNode() { return root_node_; }
 
   protected:
@@ -65,7 +68,7 @@ namespace ttrk {
   protected:
 
     void LoadFromJsonFile(const std::string &json_file);
-    void ParseJsonTree(ci::JsonTree &jt, ArticulatedNode::Ptr node);
+    void ParseJsonTree(ci::JsonTree &jt, ArticulatedNode::Ptr node, ArticulatedNode::Ptr parent, const std::string &root_dir);
 
     boost::shared_ptr<ArticulatedTree> articulated_model_;
 
@@ -74,15 +77,25 @@ namespace ttrk {
 
 
 
-  class IntuitiveSurgicalLND : ArticulatedTool {
+  class IntuitiveSurgicalLND : public ArticulatedTool {
 
   public:
 
-    IntuitiveSurgicalLND(const std::string &model_parameter_file) : ArticulatedTool(model_parameter_file) {}
+    IntuitiveSurgicalLND(const std::string &model_parameter_file) : ArticulatedTool(model_parameter_file) {
+      body_ =  articulated_model_->RootNode()->children_[0];
+      clasper_left_ = body_->children_[0];
+      clasper_right_ = body_->children_[1];
+    }
+
 
     void RotateHead(const double angle);
     void RotateClaspers(const double angle_1, const double angle_2);
 
+  protected:
+
+    ArticulatedNode::Ptr clasper_left_;
+    ArticulatedNode::Ptr clasper_right_;
+    ArticulatedNode::Ptr body_;
 
   };
 

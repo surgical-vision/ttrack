94efd95de6b12da2aec447f8e2c212af9a766555
maximilianallan <maximilian.allan@gmail.com>
2014-04-24 16:05:49 +0100

    shash error

--- src/ttrack_app.cpp
+++ src/ttrack_app.cpp
@@ -19,22 +19,14 @@ using namespace ci::app;
 
 void TTrackApp::setup(){
 
-  const std::string root_dir = "../data/lnd";
+  const std::string root_dir = "../data/new_surgeon";
 
   auto &ttrack = ttrk::TTrack::Instance();
-  ttrack.SetUp( root_dir + "/" + "model/model.json", root_dir + "/" + "camera/config.xml", root_dir + "/" + "classifier/config.xml", root_dir + "/" + "results/", ttrk::RF,ttrk::STEREO, root_dir + "/left.avi",root_dir + "/right.avi");
-  
+  //ttrack.SetUp( root_dir + "/" + "model/model.json", root_dir + "/" + "camera/config.xml", root_dir + "/" + "classifier/config.xml", root_dir + "/" + "results/", ttrk::RF,ttrk::STEREO, root_dir + "/left.avi",root_dir + "/right.avi");
+  ttrack.SetUp(root_dir + "/" + "model/model.json", root_dir + "/" + "camera/config.xml", root_dir + "/" + "classifier/config.xml", root_dir + "/" + "results/", ttrk::RF, ttrk::MONOCULAR, root_dir + "/surgery.m4v");
   time_ = getElapsedSeconds();
  
   shader_ = gl::GlslProg( loadResource( RES_SHADER_VERT ), loadResource( RES_SHADER_FRAG ) );
-
-  // load the texture
-  //  (note: enable mip-mapping by default)
-		
-  
-  //ImageSourceRef img = loadImage( loadResource( RES_DUCKY_TEX ) );
-  //if(img) mTexture = gl::Texture( img, format );
- 
   
   boost::thread main_thread( boost::ref(ttrack) );
   
@@ -56,17 +48,6 @@ void TTrackApp::update(){
   if(!ttrack.GetLatestUpdate(irs_))
     return;
   
-  cv::Mat test = cv::Mat::ones(2000,2000,CV_8UC3);
-  for(int r=0;r<test.rows;++r){
-    for(int c=0;c<test.cols;++c){
-
-      test.at<cv::Vec3b>(r,c) = cv::Vec3b(255,255,255);
-
-    }
-  }
-  ci::ImageSourceRef img = ci::fromOcv( test );//irs_->first->GetImageROI() );
-  frame_texture_ = gl::Texture( img );
-
 }
 
 void TTrackApp::draw3D() {
@@ -134,40 +115,20 @@ void TTrackApp::draw(){
 
   gl::clear( Color( 0.0f, 0.0f, 0.0f ) , true ); //set the screen to black and clear the depth buffer
   
- //draw2D();
+  draw2D();
 
-	draw3D();
+	//draw3D();
 
 }
 
 
 void TTrackApp::keyDown( KeyEvent event ){
 
-  static int frame = 0;
-
   if(event.getChar() == ' '){
 
-    //gl::Fbo framebuffer(640,480,gl::Fbo::Format());//,true,true,true);
-    //auto display = this->getDefaultRenderer();
-    //auto surface = display->copyWindowSurface(ci::Area(0,0,200,200));
-    ////ci::writeImage(surface,  
-    
+    throw(std::runtime_error("Err"));
 
-    float rand_1 = -0.5*(float)rand()/(3*RAND_MAX) + (float)rand()/(3*RAND_MAX);
-    float rand_2 = -0.5*(float)rand()/(3*RAND_MAX) + (float)rand()/(3*RAND_MAX);
-    float rand_3 = -0.5*(float)rand()/(3*RAND_MAX) + (float)rand()/(3*RAND_MAX);
-
-    
-
-    for(auto model = irs_->second.begin(); model != irs_->second.end(); ++model)  {
-      auto thing = boost::dynamic_pointer_cast<ttrk::IntuitiveSurgicalLND>(model->PtrToModel());
-      thing->RotateHead(rand_1);
-      thing->RotateClaspers(rand_2,rand_3);
-      //auto meshes_textures_transforms = model->PtrToModel()->GetRenderableMeshes();
-      //ci::Matrix44d current_pose = model->CurrentPose().AsCiMatrix();
-
-    }
-  }
+  } 
 
 
 

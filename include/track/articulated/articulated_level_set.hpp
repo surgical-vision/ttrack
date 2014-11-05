4f127820ec264f7696f68259e46b626c49962bb4
maximilianallan <maximilian.allan@gmail.com>
2014-11-05 18:04:21 +0000

    starting to add articulated code - composite sdf etc

--- include/track/articulated/articulated_level_set.hpp
+++ include/track/articulated/articulated_level_set.hpp
@@ -3,12 +3,15 @@
 
 #include "../localizer.hpp"
 #include "../pwp3d/stereo_pwp3d.hpp"
+#include "../model/node.hpp"
 
 namespace ttrk {
 
   class ArticulatedLevelSet : public Localizer {
 
   public:
+
+    ArticulatedLevelSet(boost::shared_ptr<StereoCamera> camera) : stereo_camera_(camera) {}
 
     virtual void TrackTargetInFrame(boost::shared_ptr<Model> model, boost::shared_ptr<sv::Frame> frame);
 
@@ -18,11 +21,34 @@ namespace ttrk {
 
     std::vector<StereoPWP3D> component_trackers_; /**< A selection of trackers each of which compute the level sets for each articulated component. */
     
+    ci::gl::GlslProg front_depth_;  /**< Shader to compute the front depth buffer. */
+    ci::gl::GlslProg back_depth_and_contour_;  /**< Shader to compute the back depth buffer and contour. */
+
     struct ComponentData {
 
+      cv::Mat sdf_image_;
+      cv::Mat front_intersection_image_;
+      cv::Mat back_intersection_image_;
+      
+      cv::Mat sdf_;
 
+      Node::Ptr model_;
+
+      //body (roll) -> head (w.p) -> no-geom (w.y) -> no-geom (clasper-base) -> 
+
+      //component data should mirror structure of Model-tree
+
+      //SDF for this component
+
+      //index of the component in the model (should this be a Model(Node) thing)
+
+      //jacobian could
+
+      //detection color/label mapping
 
     };
+
+    boost::shared_ptr<StereoCamera> stereo_camera_;
 
   };
 
